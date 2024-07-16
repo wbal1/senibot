155fb27a84e1e7aa37b8c4ad00093a0f238e2b83
wbal1 <81462500+wbal1@users.noreply.github.com>
2024-07-16 10:05:19 +0900

    Update dialogflow_client.py

--- dialogflow_client.py
+++ dialogflow_client.py
@@ -2,6 +2,14 @@ from google.cloud import dialogflow
 from google.oauth2 import service_account
 import os
 import json
+import uuid  # UUID를 생성하기 위한 모듈
+
+# 세션 ID 생성 함수
+def generate_session_id():
+    return str(uuid.uuid4())
+
+# 사용 예시
+u_session_id = generate_session_id()
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "credentials.json"
 credentials_info = json.loads(os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON'))
@@ -9,9 +17,9 @@ credentials = service_account.Credentials.from_service_account_info(credentials_
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
     session_client = dialogflow.SessionsClient(credentials=credentials)
-    session = session_client.session_path(project_id, session_id)
+    session = session_client.session_path('suh-3-429200', u_session_id)
     for text in texts:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+        text_input = dialogflow.TextInput(text=text, language_code='ko')
         query_input = dialogflow.QueryInput(text=text_input)
         response = session_client.detect_intent(session=session, query_input=query_input)
         return response.query_result.fulfillment_text
