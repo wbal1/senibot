cc4acbe20629e7bac9d3c5f3f5787a77913e4c23
wbal1 <81462500+wbal1@users.noreply.github.com>
2024-07-16 09:44:26 +0900

    Update dialogflow_client.py

--- dialogflow_client.py
+++ dialogflow_client.py
@@ -8,7 +8,7 @@ credentials_info = json.loads(os.getenv('GOOGLE_APPLICATION_CREDENTIALS_JSON'))
 credentials = service_account.Credentials.from_service_account_info(credentials_info)
 
 def detect_intent_texts(project_id, session_id, texts, language_code):
-    session_client = dialogflow.SessionsClient()
+    session_client = dialogflow.SessionsClient(credentials=credentials)
     session = session_client.session_path(project_id, session_id)
     for text in texts:
         text_input = dialogflow.TextInput(text=text, language_code=language_code)
